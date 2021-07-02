1231a8ca07ae90bb841246fe66b31c1219aa7474
xunxun <kim134597@gmail.com>
2021-07-02 17:43:05 +0900

    sqlll

--- NeoA/src/main/java/org/kosta/model/vo/CategoryVO.java
+++ NeoA/src/main/java/org/kosta/model/vo/CategoryVO.java
@@ -5,4 +5,45 @@ public class CategoryVO {
 	private String largeCate;
 	private String mediumCate;
 	private String smallCate;
+	public CategoryVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public CategoryVO(String categoryNo, String largeCate, String mediumCate, String smallCate) {
+		super();
+		this.categoryNo = categoryNo;
+		this.largeCate = largeCate;
+		this.mediumCate = mediumCate;
+		this.smallCate = smallCate;
+	}
+	public String getCategoryNo() {
+		return categoryNo;
+	}
+	public void setCategoryNo(String categoryNo) {
+		this.categoryNo = categoryNo;
+	}
+	public String getLargeCate() {
+		return largeCate;
+	}
+	public void setLargeCate(String largeCate) {
+		this.largeCate = largeCate;
+	}
+	public String getMediumCate() {
+		return mediumCate;
+	}
+	public void setMediumCate(String mediumCate) {
+		this.mediumCate = mediumCate;
+	}
+	public String getSmallCate() {
+		return smallCate;
+	}
+	public void setSmallCate(String smallCate) {
+		this.smallCate = smallCate;
+	}
+	@Override
+	public String toString() {
+		return "CategoryVO [categoryNo=" + categoryNo + ", largeCate=" + largeCate + ", mediumCate=" + mediumCate
+				+ ", smallCate=" + smallCate + "]";
+	}
+	
 }
